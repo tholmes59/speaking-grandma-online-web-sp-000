19decbbbc8ec45694530422b7381f5cc4c4a85a8
Tom Holmes <tholmes59@gmail.com>
2019-01-15 22:03:06 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,5 +17,6 @@ def speak_to_grandma(phrase)
   elsif phrase == phrase.uppercase
     return "NO, NOT SINCE 1938!"
   else phrase == "I LOVE YOU GRANDMA!"
-    return 
+    return "I LOVE YOU TOO PUMPKIN!"
+  end
 end
